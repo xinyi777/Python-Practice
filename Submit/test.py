8591658c456fcf5add21865ba6522cc6b5982599
Evlpsrfc <evlpsrfc@163.com>
2019-11-09 13:42:04 +0800

    rewrite some problems with LaTeX

---
+++ Submit/test.py
@@ -0,0 +1,30 @@
+def func1(k, i) -> int:
+    pass
+
+
+def func2(n) -> str:
+    pass
+
+
+def func3(lst, v) -> list:
+    pass
+
+
+def func4(k) -> int:
+    pass
+
+
+def func5(lst) -> list:
+    pass
+
+
+def func6(s) -> list:
+    pass
+
+
+def func7(s) -> list:
+    pass
+
+
+if __name__ == '__main__':
+    pass
